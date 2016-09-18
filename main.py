81f5226a866aae53555c44c012141a17c387d8f4
katesmr <ktsmrchkv@gmail.com>
2016-09-18 16:23:02 +0300

    Add GUI interface

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-import validator, RPN
+#!/usr/bin/python
+import validator
+import RPN
+from tkinter import Tk, Frame, Entry, Button, END, messagebox
+
+
+def solve(event):
+    try:
+        res = RPN.run(validator.split_expression(expression_input.get()))
+        expression_input.delete(0, END)
+        expression_input.insert(0, res)
+    except Exception as err:
+        messagebox.showerror("There was an error:", err)
 
 
 if '__main__' == __name__:
-    try:
-        print(RPN.run(validator.split_expression(input())))
-    except Exception as err:
-        print("There was an error:", err)
+    root = Tk()
+    root.resizable(width=False, height=False)
+    root.geometry('{}x{}'.format(200, 50))
+
+    input_field_frame = Frame(root)
+    input_field_frame.pack()
+    button_frame = Frame(root)
+    button_frame.pack()
+
+    expression_input = Entry(input_field_frame, width=20)
+    expression_input.pack()
+
+    run_button = Button(button_frame, text="SOLVE")
+    run_button.pack()
+    run_button.bind("<Button-1>", solve)
+
+    root.mainloop()
