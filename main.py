c24fca9b2f7fc94f3f4f4f1674a989974047776a
katesmr <ktsmrchkv@gmail.com>
2016-09-26 20:15:45 +0300

    Implement autorun for Lab task

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python
+import os
+import sys
 import validator
 import RPN
-from tkinter import Tk, Frame, Entry, Button, END, messagebox
+from tkinter import Tk, Frame, Entry, Button, END, messagebox, Checkbutton, IntVar
+
+BACK_UP_NAME = ".lres_backup"
 
 
-def solve(event):
+def on_exit(entry):
+    try:
+        with open(BACK_UP_NAME, 'w') as backup:
+            backup.write(entry.get())
+    except (OSError, IOError):
+        pass
+    root.destroy()
+
+
+def solve(_):
     try:
         res = RPN.run(validator.split_expression(expression_input.get()))
         expression_input.delete(0, END)
@@ -13,10 +26,18 @@ def solve(event):
         messagebox.showerror("There was an error:", err)
 
 
+def add():
+    try:
+        with open(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\autorunRPN.bat", "w") as autorun:
+            autorun.write("python D:\PyProjects\SAPKIS\RPNcalculator\main.py %*")
+    except (OSError, IOError) as err:
+        messagebox.showerror("There was an error:", err)
+
+
 if '__main__' == __name__:
     root = Tk()
     root.resizable(width=False, height=False)
-    root.geometry('{}x{}'.format(200, 50))
+    root.geometry('{}x{}'.format(200, 70))
 
     input_field_frame = Frame(root)
     input_field_frame.pack()
@@ -26,8 +47,22 @@ if '__main__' == __name__:
     expression_input = Entry(input_field_frame, width=20)
     expression_input.pack()
 
+    if os.path.isfile(BACK_UP_NAME):
+        try:
+            with open(BACK_UP_NAME, 'r') as backup:
+                expression_input.insert(0, backup.read())
+        except (OSError, IOError):
+            pass
+
     run_button = Button(button_frame, text="SOLVE")
-    run_button.pack()
     run_button.bind("<Button-1>", solve)
+
+    CheckVar1 = IntVar()
+    adding_button = Checkbutton(button_frame, text="Add to autoloading", variable=CheckVar1, onvalue=1, offvalue=0, command=add)
+
+    run_button.pack()
+    adding_button.pack()
+
+    root.protocol('WM_DELETE_WINDOW', lambda: on_exit(expression_input))
 
     root.mainloop()
